e07eb2d80a1521876d39c58165947e3209d52ad5
Folagbade Adeusi <zothuras@gmail.com>
2021-02-14 14:41:15 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -5,8 +5,10 @@
 def introduction(name)
   puts "Hi, my name is #{name}."
 end
+
 def introduction_with_languaget(name,language)
-  puts 
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+  
 def introduction_with_language_optional(name,language = "Ruby")
  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
