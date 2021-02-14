1d7bad73a0a39a239d015e68d02c7e6c82ebb7fc
Folagbade Adeusi <zothuras@gmail.com>
2021-02-14 14:31:46 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,7 +2,8 @@
 # the answers. You should ONLY check those answers if you get totally stuck. The important
 # thing is NOT to "complete labs," the important thing is to learn the skills you need
 # in order to complete the rest of this course and be ready for the next course!DEF
-def introduction_with_language
+def introduction_with_language(name="Dee"lanuage='Ruby')
+  
 
 
 
